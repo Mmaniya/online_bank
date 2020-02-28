677d078d88f73dbbaf5a970a3c1be021f76e9622
Manikandan M <manikandan@aaludra.com>
2020-02-28 23:28:08 +0530

    fix

--- src/app/admin/hackerhome/hackerhome.component.ts
+++ src/app/admin/hackerhome/hackerhome.component.ts
@@ -4,20 +4,6 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 
 
-export interface PeriodicElement {
-  id: string;
-  name: string;
-  acno: number;
-  hacker_ip: string;
-  date:string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {id: 'cust001', name: 'ravi', acno: 100794343, hacker_ip: '123.234.99.47', date: '2020-02-12'},
-  {id: 'cust002', name: 'kumar', acno: 443440026, hacker_ip: '132.143.39.47', date: '2020-02-11'},
-  {id: 'cust003', name: 'mani', acno: 64554941, hacker_ip: '233.4.34.44', date: '2020-01-12'},
-  {id: 'cust004', name: 'kavi', acno: 945450122, hacker_ip: '157.26.99.77', date: '2019-12-12'},
-  {id: 'cust005', name: 'brabhu', acno: 104545811, hacker_ip: '127.36.09.47', date: '2020-02-01'},
-];
 @Component({
   selector: 'app-hackerhome',
   templateUrl: './hackerhome.component.html',
@@ -25,12 +11,17 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 
 export class HackerhomeComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name', 'acno', 'hacker_ip','date'];
-  dataSource = ELEMENT_DATA;
-
+  scanList
   constructor(private router: Router,
     private route: ActivatedRoute,
-    private http: HttpClient) { }
+    private http: HttpClient) { 
+      this.http.get('http://localhost/bankauth/User/blocklist.php').subscribe(
+   
+        data => {
+          this.scanList = data as string [];	 // FILL THE ARRAY WITH DATA.
+        },
+      );
+    }
 
   ngOnInit() {
   }
